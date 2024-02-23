b0087a8cd0e73eccf19500644bdfee149d9299d9
System Administrator <sciemesfin55@gmail.com>
2024-02-23 11:26:37 +0300

    Quotaion Management

---
+++ pages/products/suppliers.tsx
@@ -0,0 +1,183 @@
+/* eslint-disable @next/next/no-img-element */
+import GetStartedButton from '@/components/getStartedButton'
+
+import React from 'react'
+
+export default function Suppliers() {
+    return (
+        <div>
+            <section className='bg-blue-600'>
+                <div className='mx-auto w-[90%] md:max-w-[90%]  pt-32 pb-8 md:pb-16'>
+                    <div className="md:flex md:items-center md:justify-between md:space-x-10">
+                        <div className='w-full md:w-1/2'>
+                            <h1 className="text-gray-200 text-white text-3xl font-semibold md:text-5xl">
+                                Streamline Procurement with {"2lifi's"}
+                            </h1>
+                            <h1 className="my-3 font-extrabold text-gray-200 text-white text-3xl md:text-7xl">
+                                Quotation Management
+                            </h1>
+
+                            <div className="md:pt-4">
+                                <GetStartedButton />
+                            </div>
+                        </div>
+                        <div className='w-full md:w-1/2 py-10 md:py-1'>
+                            <img
+                                src="https://img.freepik.com/free-vector/professional-dashboard-user-panel_23-2148359902.jpg?size=626&ext=jpg&uid=R19609540&ga=GA1.1.617832925.1708503188&semt=ais"
+                                className='rounded-xl'
+                                alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section >
+                <div className='mx-auto w-[90%] md:max-w-[90%]  py-10'>
+                    <div className="md:flex items-center justify-center md:space-x-10">
+                        <div
+                            data-aos="fade-up"
+                            data-aos-duration="500"
+                            className='w-full md:w-1/2 py-10 md:py-1'>
+                            <h1 className='text-2xl md:text-4xl font-normal text-blue-600'>
+                                Request for Quotation (RFQ)
+                            </h1>
+                            <p className='text-[#676c72] pt-4 py-2'>
+                                In the realm of procurement, the Request for Quotation (RFQ)
+                                serves as the initial step in sourcing goods or services from
+                                suppliers. With {"2lifi's"} intuitive RFQ module, businesses
+                                can streamline their procurement processes effortlessly.
+                                By generating RFQs directly within the ERP system, organizations
+                                can solicit competitive bids from multiple vendors, ensuring
+                                transparency and cost-effectiveness. From defining specific
+                                requirements to tracking supplier responses, {"2lifi's"} RFQ
+                                feature empowers users to efficiently manage the entire quotation
+                                process, ultimately facilitating informed decision-making and
+                                fostering fruitful supplier relationships.
+                            </p>
+                            <GetStartedButton />
+                        </div>
+                        <div
+
+                            className='w-full shadow rounded md:w-1/2 '>
+                            <img src="https://docs.erpnext.com/files/rfq-create.png" alt=""
+                                className='object-cover rounded h-full w-full'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className='bg-gray-100'>
+                <div className='mx-auto w-[90%] md:max-w-[90%] py-10'>
+                    <div className="md:flex items-center justify-center md:space-x-10">
+                        <div
+
+                            className='w-full shadow rounded md:w-1/2'>
+                            <img src="https://docs.erpnext.com/files/P010Tfe.png" alt=""
+                                className='object-cover rounded h-full w-full'
+                            />
+                        </div>
+                        <div
+                            data-aos="fade-up"
+                            data-aos-duration="500"
+                            className='w-full md:w-1/2 py-10 md:py-1'>
+                            <h1 className='text-2xl md:text-4xl font-normal text-blue-600'>
+                                Supplier Quotation
+                            </h1>
+                            <p className='text-[#676c72] pt-4 py-2'>
+                                Efficient supplier management lies at the heart of successful
+                                procurement operations, and {"2lifi's"} Supplier Quotation module
+                                offers a comprehensive solution for businesses seeking to optimize
+                                their sourcing strategies. By centralizing supplier communications
+                                and quotations within the Enterprise system, organizations can easily
+                                compare pricing, delivery terms, and other crucial factors to identify
+                                the most favorable offers. With features such as automated quotation
+                                requests and customizable approval workflows, 2lifi empowers users to
+                                streamline collaboration with suppliers while maintaining complete
+                                visibility into the quotation lifecycle. From initial negotiation to
+                                final acceptance, {"2lifi's"} Supplier Quotation module facilitates
+                                seamless procurement processes, enabling businesses to secure the best
+                                value for their investments.
+                            </p>
+                            <GetStartedButton />
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section >
+                <div className='mx-auto w-[90%] md:max-w-[90%]  py-10'>
+                    <div className="md:flex items-center justify-center md:space-x-10">
+                        <div
+                            data-aos="fade-up"
+                            data-aos-duration="500"
+                            className='w-full md:w-1/2 py-10 md:py-1'>
+                            <h1 className='text-2xl md:text-4xl font-normal text-blue-600'>
+                                Customer Quotations
+                            </h1>
+                            <p className='text-[#676c72] pt-4 py-2'>
+                                A quotation is an estimated cost of the products/services {"you're"}
+                                selling to your future/present customer. <br />
+                                In the realm of sales and customer relationship management,
+                                providing accurate and timely quotations is paramount to securing
+                                business opportunities and fostering client trust. With {"2lifi's "}
+                                Customer Quotations module, businesses can effortlessly generate
+                                professional quotes tailored to individual customer needs while
+                                ensuring consistency and compliance with pricing policies.
+                            </p>
+                            <GetStartedButton />
+                        </div>
+                        <div
+
+                            className='w-full shadow rounded md:w-1/2 py-10 md:py-1'>
+                            <img src="https://docs.erpnext.com/files/rfq-create.png" alt=""
+                                className='object-cover rounded h-full w-full'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section className='bg-gray-100'>
+                <div className='mx-auto w-[90%] md:max-w-[90%]  py-10'>
+                    <h1 className='text-4xl py-10 md:text-6xl font-extrabold  text-center'>
+                        Precision, Efficiency, Success!
+                    </h1>
+                    <div className="md:flex items-center justify-center md:space-x-10">
+                        <div
+
+                            className='w-full shadow rounded md:w-1/2'>
+                            <img src="https://img.freepik.com/free-vector/dashboard-user-panel-template_52683-29590.jpg?size=626&ext=jpg&uid=R19609540&ga=GA1.1.617832925.1708503188&semt=ais" alt=""
+                                className='object-cover rounded h-full w-full'
+                            />
+                        </div>
+                        <div
+                            data-aos="fade-up"
+                            data-aos-duration="500"
+                            className='w-full md:w-1/2 py-10 md:py-1'>
+                            <h1 className='text-2xl md:text-4xl font-normal text-blue-600'>
+                                Where Precision Meets Efficiency for Seamless Sourcing and Sales Success!
+                            </h1>
+                            <p className='text-[#676c72] pt-4 py-2'>
+                                Experience the future of efficient quotation management with
+                                {"2lifi's"} Quotation module. From sourcing the best deals to
+                                closing lucrative sales, streamline your processes with customizable
+                                templates, automated workflows, and real-time tracking. Say goodbye
+                                to manual tasks and hello to increased productivity and customer
+                                satisfaction. Unlock greater profitability today with {"2lifi's"}
+                                Quotation module.
+                            </p>
+                            <GetStartedButton />
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+
+
+        </div>
+    )
+}
