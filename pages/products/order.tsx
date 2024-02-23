646e9230d6ab06f567574ab79948bdcb0c12e57a
System Administrator <sciemesfin55@gmail.com>
2024-02-23 12:52:36 +0300

    Update Buying

--- pages/products/order.tsx
+++ pages/products/order.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import GetStartedButton from '@/components/getStartedButton'
+import Link from 'next/link'
 
 import React from 'react'
 
-export default function Suppliers() {
+export default function Order() {
     return (
         <div>
             <section className='bg-blue-600'>
@@ -11,16 +12,18 @@ export default function Suppliers() {
                     <div className="md:flex md:items-center md:justify-between md:space-x-10">
                         <div className='w-full md:w-1/2'>
                             <h1 className="my-3 font-extrabold text-gray-200 text-white text-3xl md:text-7xl">
-                                Empower Your Supply Chain with 2lifi
+                                Simplicity Unleashed !
                             </h1>
-
+                            <h1 className="text-gray-200 text-white text-3xl md:text-5xl pt-2">
+                                Streamlining Order Management with 2lifi
+                            </h1>
                             <div className="md:pt-4">
                                 <GetStartedButton />
                             </div>
                         </div>
                         <div className='w-full md:w-1/2 py-10 md:py-1'>
                             <img
-                                src="https://img.freepik.com/free-photo/digital-tablet-online-learning_53876-97299.jpg?size=626&ext=jpg&uid=R19609540&ga=GA1.1.617832925.1708503188&semt=ais"
+                                src="https://img.freepik.com/free-vector/dashboard-user-panel_23-2148390498.jpg?size=626&ext=jpg&uid=R19609540&ga=GA1.1.617832925.1708503188&semt=ais"
                                 className='rounded-xl'
                                 alt="" />
                         </div>
@@ -43,14 +46,15 @@ export default function Suppliers() {
                             data-aos-duration="500"
                             className='w-full md:w-1/2 py-10 md:py-1'>
                             <h1 className='text-2xl md:text-4xl font-normal text-blue-600'>
-                                Supplier
+                                2lifi Order
                             </h1>
                             <p className='text-[#676c72] pt-4 py-2'>
-                                Suppliers are companies or individuals who provide you with products or services.
-                                Suppliers are the cornerstone of efficient supply chain management,
-                                providing the raw materials, products, and services that businesses
-                                rely on to thrive. At 2lifi, we recognize the critical role suppliers
-                                play in sustaining operations and fostering growth.
+                                In the realm of business operations, orders serve as the cornerstone
+                                of transactions, representing the exchange of goods and services between
+                                businesses and their customers or suppliers. At 2lifi, we recognize the
+                                pivotal role that orders play in driving the seamless flow of commerce,
+                                which is why we offer robust solutions designed to streamline order
+                                management processes, enhance accuracy, and improve overall efficiency.
                             </p>
                             <GetStartedButton />
                         </div>
@@ -67,28 +71,21 @@ export default function Suppliers() {
                             data-aos-duration="500"
                             className='w-full md:w-1/2 py-10 md:py-1'>
                             <h1 className='text-2xl md:text-4xl font-normal text-blue-600'>
-                                Suppliers Portal
+                                Sales Order
                             </h1>
                             <p className='text-[#676c72] pt-4 py-2'>
-                                The 2lifi Suppliers Portal redefines collaboration between
-                                businesses and their suppliers, offering a centralized platform
-                                for seamless communication, transaction management, and
-                                information exchange. Through the Suppliers Portal, suppliers
-                                gain direct access to critical data, including purchase orders,
-                                invoices, and inventory levels, enabling faster response times and
-                                greater visibility into supply chain operations. With customizable
-                                dashboards and secure authentication, the Suppliers Portal
-                                ensures a user-friendly experience while maintaining the highest
-                                standards of data security. By facilitating transparent communication
-                                and streamlined workflows, 2lifi empowers businesses to optimize
-                                supplier relationships and drive mutual success.
+                                A Sales Order is a confirmation of an order from your customer.
+                                With {"2lifi's"} Sales Orders module, businesses gain access to a
+                                powerful platform that facilitates the entire sales order
+                                lifecycle—from creation and modification to fulfillment and invoicing.
+                                <Link href="/products/selling" className='text-blue-600 px-1'>Learn more</Link>
                             </p>
                             <GetStartedButton />
                         </div>
                         <div
 
                             className='w-full shadow rounded md:w-1/2 '>
-                            <img src="https://img.freepik.com/free-vector/dashboard-user-panel-concept_23-2148424696.jpg?size=626&ext=jpg&uid=R19609540&ga=GA1.1.617832925.1708503188&semt=ais" alt=""
+                            <img src="https://docs.erpnext.com/files/so-packing-list.png" alt=""
                                 className='object-cover rounded h-full w-full'
                             />
                         </div>
@@ -102,7 +99,7 @@ export default function Suppliers() {
                         <div
 
                             className='w-full shadow rounded md:w-1/2'>
-                            <img src="https://docs.erpnext.com/files/P010Tfe.png" alt=""
+                            <img src="https://docs.erpnext.com/files/purchase-order.png" alt=""
                                 className='object-cover rounded h-full w-full'
                             />
                         </div>
@@ -111,22 +108,19 @@ export default function Suppliers() {
                             data-aos-duration="500"
                             className='w-full md:w-1/2 py-10 md:py-1'>
                             <h1 className='text-2xl md:text-4xl font-normal text-blue-600'>
-                                Supplier Quotation
+                                Purchase Order
                             </h1>
                             <p className='text-[#676c72] pt-4 py-2'>
-                                Efficient supplier management lies at the heart of successful
-                                procurement operations, and {"2lifi's"} Supplier Quotation module
-                                offers a comprehensive solution for businesses seeking to optimize
-                                their sourcing strategies. By centralizing supplier communications
-                                and quotations within the Enterprise system, organizations can easily
-                                compare pricing, delivery terms, and other crucial factors to identify
-                                the most favorable offers. With features such as automated quotation
-                                requests and customizable approval workflows, 2lifi empowers users to
-                                streamline collaboration with suppliers while maintaining complete
-                                visibility into the quotation lifecycle. From initial negotiation to
-                                final acceptance, {"2lifi's"} Supplier Quotation module facilitates
-                                seamless procurement processes, enabling businesses to secure the best
-                                value for their investments.
+                                A Purchase Order is a binding contract with your Supplier that you
+                                promise to buy a set of items under given conditions.
+                                At 2lifi, we understand the critical role that purchase orders play
+                                in effective supply chain management, which is why we offer comprehensive
+                                solutions designed to streamline the procurement process and
+                                enhance visibility across the entire supply chain.
+                                With {"2lifi's"} Purchase Orders module, businesses can easily create,
+                                track, and manage purchase orders, automate approval workflows, and collaborate
+                                seamlessly with suppliers, thereby improving procurement efficiency,
+                                reducing lead times, and driving bottom-line results.
                             </p>
                             <GetStartedButton />
                         </div>
@@ -143,35 +137,30 @@ export default function Suppliers() {
                             data-aos-duration="500"
                             className='w-full md:w-1/2 py-10 md:py-1'>
                             <h1 className='text-2xl md:text-4xl font-normal text-blue-600'>
-                                Supplier Management
+                                Effortless Order Management
                             </h1>
                             <p className='text-[#676c72] pt-4 py-2'>
-                                Effective supplier management is essential for optimizing
-                                procurement processes, minimizing risks, and maintaining
-                                high standards of quality and reliability. With {"2lifi's"}
-                                Supplier Management module, businesses gain comprehensive oversight
-                                and control over their supplier relationships. From vendor evaluation
-                                and qualification to contract management and performance tracking,
-                                our intuitive system empowers organizations to make informed
-                                decisions and foster strategic partnerships. With real-time
-                                insights and automated workflows, 2lifi simplifies supplier
-                                management, enabling businesses to drive operational excellence
-                                and achieve sustainable growth.
+                                The Order module in 2lifi offers unparalleled simplicity.
+                                With its intuitive interface, users can easily create, track,
+                                and manage orders in real-time. Customizable workflows and
+                                automatic notifications streamline processes, empowering
+                                businesses to optimize operations and enhance customer satisfaction
+                                effortlessly.
                             </p>
                             <GetStartedButton />
                         </div>
                         <div
 
-                            className='w-full shadow rounded md:w-1/2 py-10 md:py-1'>
-                            <img src="https://docs.erpnext.com/files/supplier-master.png" alt=""
-                                className='object-cover rounded h-full w-full'
+                            className='w-full rounded md:w-1/2 py-10 md:py-1 '>
+                            <img
+                                src="https://img.freepik.com/free-psd/flat-design-transport-delivery-template_23-2149558451.jpg?t=st=1708681028~exp=1708684628~hmac=f544f56ea6afc04ceb8c3f3d559d7960d9b0662a8908776510734004966f92e4&w=1800"
+                                alt=""
+                                className='object-cover rounded h-full w-full max-h-[300px]'
                             />
                         </div>
                     </div>
                 </div>
             </section>
-
-
 
 
         </div>
